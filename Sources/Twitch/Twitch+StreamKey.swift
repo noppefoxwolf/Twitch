5360554e6b84d82c9aa14d1bf44d7ccd449980b3
noppe <noppelabs@gmail.com>
2020-09-18 04:04:49 +0900

    fix stream_key api

--- Sources/Twitch/Twitch+StreamKey.swift
+++ Sources/Twitch/Twitch+StreamKey.swift
@@ -10,7 +10,9 @@ import Combine
 
 public extension Twitch.Client {
     func getStreamKey(broadcasterID: String) -> AnyPublisher<GetStreamKey.Response, Error> {
-        self.get(path: "/helix/streams/key", params: ["broadcaster_id" : broadcasterID])
+        self.get(path: "/helix/streams/key", queryItems: [
+            .init(name: "broadcaster_id", value: broadcasterID)
+        ])
     }
     
     enum GetStreamKey {
--- Sources/Twitch/Twitch.swift
+++ Sources/Twitch/Twitch.swift
@@ -2,7 +2,7 @@ import Foundation
 import Combine
 
 public enum Twitch {
-    static let baseURL: URL = URL(string: "https://api.twitch.tv/")!
+    static let baseURL: URL = URL(string: "https://api.twitch.tv")!
     
     public struct Client {
         let clientID: String
@@ -13,18 +13,16 @@ public enum Twitch {
             self.accessToken = accessToken
         }
         
-        func get<T: Codable>(path: String, params: [String : String] = [:]) -> AnyPublisher<T, Error> {
-            let query = params.map({ "\($0.key)=\($0.value)" }).joined(separator: "&")
-            var url = Twitch.baseURL.appendingPathComponent(path)
-            if !query.isEmpty {
-                url.appendPathComponent("?" + query)
-            }
-            print(url)
-            var request = URLRequest(url: url)
+        func get<T: Codable>(path: String, queryItems: [URLQueryItem] = []) -> AnyPublisher<T, Error> {
+            let endpoint = Twitch.baseURL.appendingPathComponent(path)
+            var urlComponents = URLComponents(url: endpoint, resolvingAgainstBaseURL: false)!
+            urlComponents.queryItems = queryItems
+            var request = URLRequest(url: urlComponents.url!)
             request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
             request.setValue(clientID, forHTTPHeaderField: "client-id")
             let session = URLSession(configuration: .default)
             return session.dataTaskPublisher(for: request).tryMap { (data, response) -> T in
+                print(String.init(data: data, encoding: .utf8))
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 do {
